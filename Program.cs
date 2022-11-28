d4495ace7bfcecc43123e81645cba6c21459a357
Temimeno <kittipitch.mold@mail.kmutt.ac.th>
2022-11-28 12:38:36 +0700

    Edit Program.cs to be able to run Timer.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Timers;
+class Program {
+
+    public static System.Timers.Timer aTimer;
+
+    public static void Main(string[] args) {
+
+        TimeCounter timeCounter = new TimeCounter();
+
+        timeCounter.Runner();
+   }
+}
